9254a65c5936fa04a81bb980f8efe5aaa1258879
ivanaaaz <ivana.zivaljevic.95@hotmail.com>
2021-08-25 00:55:00 +0200

    Styling list

--- components/MemberList/index.js
+++ components/MemberList/index.js
@@ -4,7 +4,7 @@ import '../../tailwind.config';
 
 const Member = ({ member }) => {
   return (
-    <li className="px-16 py-16 text-center border-b border-gray-300">
+    <li className="px-16 py-16 text-center text-xl">
       <Link href={'/members/' + member.id}>
         <a>
           <p>{member.name}</p>
@@ -16,11 +16,12 @@ const Member = ({ member }) => {
 
 const MemberList = ({ members }) => {
   return (
-    <ul className="bg-secondary-50 mt-20 mx-80 my-80 mb-40 text-lg">
-      {members.map((member) => (
-        <Member key={member.id} member={member} />
-      ))}
-    </ul>
+      <ul className="grid grid-rows-4 gap-10 bg-secondary-50 bg-opacity-50 mt-20 mx-80 my-80 mb-40 text-lg p-8 rounded-3xl divide-y divide-gray-300 shadow-xl">
+        {members.map((member) => (
+          <Member key={member.id} member={member} />
+        ))}
+      </ul>
+   
   );
 };
 
