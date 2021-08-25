d5cbec90333ab1a4ff167e5c0707437bfae8fcab
ivanaaaz <ivana.zivaljevic.95@hotmail.com>
2021-08-25 18:21:24 +0200

    Responsive Details page

--- pages/members/[id].js
+++ pages/members/[id].js
@@ -8,18 +8,18 @@ export default function MemberDetails() {
   if (!member) return null;
   return (
     <div>
-      <h1 className="text-secondary-300 text-6xl text-center px-16 py-16">
+      <h1 className="mt-10 md:mt-16 mb-6 lg:mt-20 text-secondary-300 lg:text-6xl md:text-5xl text-4xl text-center px-16 py-16">
         Details of members
       </h1>
-      <ul className="mt-2 mx-80 my-80 mb-10 text-lg">
-        <li className="px-16 py-16 text-center border-b border-gray-300 text-3xl text-bold text-gray-600 pb-10">
+      <ul className="m-6 mb-20 md:m-20 md:mb-40 lg:mt-20 lg:ml-60 lg:mr-60 lg:mb-40">
+        <li className="px-16 py-16 text-center border-b border-gray-300 lg:text-3xl md:text-2xl text-xl text-bold text-gray-600 pb-10">
           <p>{member.name}</p>
         </li>
-        <li className="px-16 py-16 text-center text-base pt-8">
+        <li className="px-16 py-16 text-center lg:text-base md:text-sm text-xs pt-8">
           <p>{member.email}</p>
         </li>
       </ul>
-      <p className="flex justify-center h-80 width-full object-cover mb-40 mt-10">
+      <p className="flex justify-center h-52 md:h-64 lg:h-80 width-full object-cover mb-40 mt-10">
         {/*eslint-disable-next-line*/}
         <img src={member.image} alt="profile" />
       </p>
