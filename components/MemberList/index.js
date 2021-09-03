cbdc52d8479f5f41a986c99fb5cfad2394c8fb12
ivanaaaz <ivana.zivaljevic.95@hotmail.com>
2021-09-03 11:31:42 +0400

    table on the home page

--- components/MemberList/index.js
+++ components/MemberList/index.js
@@ -2,23 +2,54 @@ import Link from 'next/link';
 
 const Member = ({ member }) => {
   return (
-    <li className="px-16 py-16 text-center">
-      <Link href={'/members/' + member.id}>
-        <a>
-          <p>{member.name}</p>
-        </a>
-      </Link>
-    </li>
+    <Link href={'/members/' + member.id}>
+      <a>
+        <td className="px-6 py-4 whitespace-nowrap">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 h-10 w-10">
+              {/*eslint-disable-next-line*/}
+              <img
+                className="h-10 w-10 rounded-full"
+                src={member.image}
+                alt=""
+              />
+            </div>
+            <div className="ml-4">
+              <div className="text-sm font-medium text-gray-900">
+                {member.name}
+              </div>
+              <div className="text-sm text-gray-500">{member.email}</div>
+            </div>
+          </div>
+        </td>
+      </a>
+    </Link>
   );
 };
 
 const MemberList = ({ members }) => {
   return (
-    <ul className="text-xl m-10 md:m-20 lg:m-40 bg-red-50 lg:text-3xl md:text-2xl p-8 rounded-3xl divide-y divide-gray-300 shadow-xl text-gray-700">
-      {members.map((member) => (
-        <Member key={member.id} member={member} />
-      ))}
-    </ul>
+    <table className="text-xl m-10 md:m-20 lg:m-40 bg-red-50 lg:text-3xl md:text-2xl p-8 rounded-3xl divide-y divide-gray-300 shadow-xl text-gray-700">
+      <thead className="bg-gray-50">
+        {' '}
+        <tr>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+            Name
+          </th>
+        </tr>
+      </thead>
+
+      <tbody className="bg-white divide-y divide-gray-200">
+        <tr>
+          {members.map((member) => (
+            <Member key={member.id} member={member} />
+          ))}
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
