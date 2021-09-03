a2995529276dced678531b400167a2a9a42dc5c2
ivanaaaz <ivana.zivaljevic.95@hotmail.com>
2021-09-03 08:39:05 +0200

    Name as tittle

--- pages/members/[id].js
+++ pages/members/[id].js
@@ -8,14 +8,11 @@ export default function MemberDetails() {
   if (!member) return null;
   return (
     <div>
-      <h1 className="mt-10 md:mt-16 mb-6 lg:mt-20 text-red-300 lg:text-6xl md:text-5xl text-4xl text-center px-16 py-16">
-        Details of members
+      <h1 className="mt-10 md:mt-16 mb-6 lg:mt-20 text-red-300 lg:text-6xl md:text-5xl text-4xl text-center lg:px-8 lg:py-8">
+       {member.name}
       </h1>
-      <ul className="m-6 mb-20 md:m-20 md:mb-40 lg:mt-20 lg:ml-60 lg:mr-60 lg:mb-40">
-        <li className="px-16 py-16 text-center border-b border-gray-300 lg:text-3xl md:text-2xl text-xl text-bold text-gray-600 pb-10">
-          <p>{member.name}</p>
-        </li>
-        <li className="px-16 py-16 text-center lg:text-base md:text-sm text-xs pt-8">
+      <ul className="m-6 mb-20 md:m-20 md:mb-40 lg:mt-8 lg:ml-60 lg:mr-60 lg:mb-40">
+        <li className="px-8 py-8 text-center lg:text-base md:text-sm text-xs pt-2">
           <p>{member.email}</p>
         </li>
       </ul>
