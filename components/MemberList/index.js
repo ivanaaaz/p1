8c610f01ba1db75298d831d6974dee556d84b96b
Osman Erdem Asga <erdem.asga@gmail.com>
2021-09-05 19:03:38 +0200

    input change, search state

--- components/MemberList/index.js
+++ components/MemberList/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import Card from '../Card';
 import Input from '../Input';
@@ -29,33 +30,47 @@ const Member = ({ member }) => {
   );
 };
 
+const Table = ({ members }) => {
+  return (
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+            Name
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        {members.map((member) => (
+          <tr key={member.id}>
+            <Member member={member} />
+          </tr>
+        ))}
+        <tr></tr>
+      </tbody>
+    </table>
+  );
+};
+
 const MemberList = ({ members }) => {
+  const [searchText, setSearchText] = useState('');
   return (
     <div>
       <div className="pb-2 mb-8 border-b-2">
-        <Input type="search" />
+        <Input
+          type="search"
+          name="searchInput"
+          value={searchText}
+          onChange={(event) => {
+            setSearchText(event.target.value);
+          }}
+        />
       </div>
       <Card>
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Name
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {members.map((member) => (
-              <tr key={member.id}>
-                <Member member={member} />
-              </tr>
-            ))}
-            <tr></tr>
-          </tbody>
-        </table>
+        <Table members={members} />
       </Card>
     </div>
   );
